7d323895ab02167e28aff8fe7ad129ecb1672b85
Vikas Nayak <102694776+vikas-nayak@users.noreply.github.com>
2024-11-02 14:11:57 +0530

    localhost changed to taskflow domain

--- src/app/api/auth/callback/notion/route.ts
+++ src/app/api/auth/callback/notion/route.ts
@@ -42,10 +42,10 @@ export async function GET(req: NextRequest) {
         console.log(databaseId)
 
       return NextResponse.redirect(
-        `https://localhost:3000/dashboard/connections?access_token=${response.data.access_token}&workspace_name=${response.data.workspace_name}&workspace_icon=${response.data.workspace_icon}&workspace_id=${response.data.workspace_id}&database_id=${databaseId}`
+        `https://taskflow-gamma-lime.vercel.app/dashboard/connections?access_token=${response.data.access_token}&workspace_name=${response.data.workspace_name}&workspace_icon=${response.data.workspace_icon}&workspace_id=${response.data.workspace_id}&database_id=${databaseId}`
       );
     }
   }
 
-  return NextResponse.redirect('https://localhost:3000/dashboard/connections');
+  return NextResponse.redirect('https://taskflow-gamma-lime.vercel.app/dashboard/connections');
 }
